22a0089dd81b3ee3bc4be72f4544f217d60fc708
Ganesh Pise <piseganesh123@gmail.com>
2021-09-27 14:13:48 +0530

    updated tf scripts

---
+++ jenkins-config.sh
@@ -0,0 +1,39 @@
+
+#! /bin/bash
+
+
+echo " ======= Instal kubectl ============"
+
+#instal common utilities
+#install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+echo " ======= Instal Docker ============"
+
+#curl -LO https://github.com/prometheus/prometheus/releases/download/v2.0.0/prometheus-2.0.0.linux-amd64.tar.gz
+
+#install docker
+
+ sudo apt-get update
+ sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo " ======= Instal Jenkins ============"
+
+
+
---
+++ jenkins-mst-vm.tf
@@ -0,0 +1,73 @@
+// Create VM for Ansible client
+// Configure the Google Cloud provider
+
+variable "project_id" {
+  type = string
+  default = "vast-pad-319812"
+}
+variable "region" {
+  type = string
+  default = "asia-south1"
+}
+provider "google" {
+ credentials = file("/home/piseg432/keys/gce-creator.json")
+// credentials =  $(GOOGLE_CLOUD_KEYFILE_JSON)
+ //project     = "vast-pad-31912"
+ project     = var.project_id
+  region      = var.region
+}
+
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
+  project = var.project_id
+  region = var.region
+//  depends_on = [ google_compute_firewall.firewall ]
+}
+// A single Compute Engine instance
+resource "google_compute_instance" "gcp-instance" {
+ // name         = "prografana-poc-vm-${random_id.instance_id.hex}"
+ name = "jenkins-mst-tf"
+ machine_type = "e2-medium"
+ zone         = "asia-south1-c"
+ tags = ["allow-jenkins-8080-5000","http-server"]
+  labels = {
+    "purpose" = "poc"
+    "preserve" = "no"
+  }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20210720"
+    }
+  }
+
+// 
+// metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync"
+  metadata_startup_script = file("jenkins-config.sh")
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     nat_ip = google_compute_address.static.address
+     }
+  }
+  metadata = {
+   ssh-keys = "piseg432:${file("~/.ssh/id_rsa.pub")}"
+  }
+  /* provisioner "file" {
+        source = "/home/piseg432/.kube/config"
+        destination = "/home/piseg432/.config"
+        connection {
+            type = "ssh"
+            user = "piseg432"
+            private_key = file("~/.ssh/id_rsa")
+            host = google_compute_address.static.address
+        }
+    } */
+}
+
+output "instance_ip_addr" {
+  value = "${google_compute_instance.gcp-instance.hostname}"
+}
+

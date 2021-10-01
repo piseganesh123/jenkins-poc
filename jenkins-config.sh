bed407f92b23a812a444536ab0dcec0847522b59
Ganesh Pise <piseganesh123@gmail.com>
2021-10-01 11:16:13 +0530

    updated jenkinsfil

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -1,19 +1,9 @@
 
 #! /bin/bash
 
-
-echo " ======= Instal kubectl ============"
-
 #instal common utilities
-#install Kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-#wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
 echo " ======= Instal Docker ============"
-
-#curl -LO https://github.com/prometheus/prometheus/releases/download/v2.0.0/prometheus-2.0.0.linux-amd64.tar.gz
 
 #install docker
 
@@ -37,5 +27,12 @@ echo " ======= Instal Jenkins ============"
 
 sudo docker pull jenkins/jenkins:2.303.1
 
-sudo docker run -p 80:8080 -p 50000:50000 -d -u root -v /home/piseg432/jenkins-home:/var/jenkins_home jenkins/jenkins:2.303.1
+mkdir -p /opt/jenkins_home
 
+sudo docker run -p 80:8080 -p 50000:50000 -d -u root -v /opt/jenkins_home:/var/jenkins_home jenkins/jenkins:2.303.1
+
+#install Kubectl
+echo " ======= Instal kubectl ============"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl

b1679257f0e4bc54c41dcbea61c89b3a9aa14999
piseganesh123 <piseganesh123@gmail.com>
2021-12-13 14:01:33 +0530

    added docker-compose

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -36,3 +36,8 @@ echo " ======= Instal kubectl ============"
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#install Docker-compose
+  #install docker-compose. This is latest version need to update script to install specific required version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
